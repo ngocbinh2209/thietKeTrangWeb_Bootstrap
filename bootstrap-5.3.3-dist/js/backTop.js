7b8443813780686b84427adf46851e8bbe7d3040
wildhorse22 <ngocbinh220923@gmail.com>
2025-02-18 19:07:46 +0700

    up1

---
+++ bootstrap-5.3.3-dist/js/backTop.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop()) {
+            $('#backTop').fadeIn();
+        } else {
+            $('#backTop').fadeOut();
+        }
+    });
+    $("#backTop").click(function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 300);
+    });
+});
